834ec5d9f6056ca4c55d6e90f0611c6cf8750bf5
Mark Glenn <markglenn@gmail.com>
2012-07-09 22:26:59 -0500

    Removed last remnants of mobylette

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-  include Mobylette::RespondToMobileRequests
   protect_from_forgery
-
-  mobylette_config do |config|
-    config[:skip_xhr_requests] = false
-  end
 end
